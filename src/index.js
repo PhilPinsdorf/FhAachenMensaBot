7d507c5033b5f0e1aff32a36f8eb6ef3ef5d7050
Phil Pinsdorf <philemon.pinsdorf@live.de>
2022-10-24 10:13:18 +0200

    Quick Message Dump

--- src/index.js
+++ src/index.js
@@ -41,6 +41,8 @@ mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
                     startBot((result) => {
                         console.log('Started Telegram Bot');
 
+                        sendMessages();
+
                         // Chron Jobs jeweils minus 2 Stunden wegen anderer Zeitzone
 
                         schedule.scheduleJob('30 7 * * 1-5', () => { sendMessages() })
